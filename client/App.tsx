7cb9597117376b7fda459c36a92bc750534ec528
jaenixlee <jaeni_lee@yahoo.com>
2023-05-08 17:12:12 -0400

    created frontend components

--- client/App.tsx
+++ client/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Login from './components/Login';
+import { Box } from '@mui/material';
+import Moods from './components/Moods';
+import Create from './components/Create';
 
 export default function App() {
     return (
-        <div>Hello World</div>
+        <Box sx={{margin: 'auto'}}>
+            {/* <Login /> */}
+            {/* <Create /> */}
+            <Moods />
+        </Box>
     )
 };
